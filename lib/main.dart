d1678b37ab869f7fa5c6a99862730917dde4caf6
Christian Pannwitz <ch.von.pa@googlemail.com>
2019-11-05 22:35:24 +0100

    added simple routes

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,26 @@ class App extends StatelessWidget {
   Widget build(context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.indigo),
-      home: Scaffold(
+      routes: mainRoutes,
+      // home: HomeRoute()
+    );
+  }
+}
+
+class HomeRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
           appBar: AppBar(
+            leading: Builder(
+              builder: (BuildContext context) {
+                return IconButton(
+                  icon: const Icon(Icons.menu),
+                  onPressed: () => Navigator.of(context).pushNamed('/settings'),
+                );
+              },
+            ),
             title: Text('Products Test App'),
           ),
           body: ListView(
@@ -21,3 +39,29 @@ class App extends StatelessWidget {
     );
   }
 }
+
+class SettingsRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () => Navigator.of(context).pushNamed('/'),
+              );
+            },
+          ),
+          title: Text('Products Test App'),
+        ),
+      ),
+    );
+  }
+}
+
+Map<String, Widget Function(BuildContext)> mainRoutes = {
+  '/': (context) => HomeRoute(),
+  '/settings': (context) => SettingsRoute()
+};
